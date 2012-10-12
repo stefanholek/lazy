141c12ee98b362c9f23cd03ee67b50ca5243baa0
Stefan H. Holek <stefan@epy.co.at>
2012-10-12 23:48:23 +0200

    Sync with readthedocs branch.

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,6 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
 html_theme = 'nature'
 html_style = 'nature-plus.css'
 
