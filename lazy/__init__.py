8d3dcec336357f419ad3fca29c38f6ba76837636
Stefan H. Holek <stefan@epy.co.at>
2022-05-15 21:26:05 +0200

    Re-export attribute via __all__.
    This feels less obscure than the "as" syntax.
    https://mypy.readthedocs.io/en/stable/config_file.html#confval-implicit_reexport

--- lazy/__init__.py
+++ lazy/__init__.py
@@ -1,4 +1,6 @@
 """The lazy module."""
 
 from __future__ import absolute_import
-from .lazy import lazy as lazy  # Re-export attribute
+from .lazy import lazy
+
+__all__ = ["lazy"]  # Re-export attribute
