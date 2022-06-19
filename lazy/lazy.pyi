39128a62a79b86cdd8ed7e59797cc5633d846ce0
Stefan H. Holek <stefan@epy.co.at>
2022-06-19 16:58:13 +0200

    Replace TypeVar("_C") with Any and object to make subclassing work.

--- lazy/lazy.pyi
+++ lazy/lazy.pyi
@@ -1,20 +1,20 @@
 from typing import TypeVar, Callable, Type, Generic
-from typing import Optional, overload
+from typing import Optional, Any, overload
 
 _R = TypeVar("_R")
-_C = TypeVar("_C")
 
 
-class lazy(Generic[_R, _C]):
+class lazy(Generic[_R]):
+    __func: Callable[[Any], _R]
 
-    def __init__(self, func: Callable[[_C], _R]) -> None: ...
-
-    @overload
-    def __get__(self, inst: None, inst_cls: Optional[Type[_C]] = ...) -> lazy[_R, _C]: ...
+    def __init__(self, func: Callable[[Any], _R]) -> None: ...
 
     @overload
-    def __get__(self, inst: _C, inst_cls: Optional[Type[_C]] = ...) -> _R: ...
+    def __get__(self, inst: None, inst_cls: Optional[Type[Any]] = ...) -> lazy[_R]: ...
+
+    @overload
+    def __get__(self, inst: object, inst_cls: Optional[Type[Any]] = ...) -> _R: ...
 
     @classmethod
-    def invalidate(cls, inst: _C, name: str) -> None: ...
+    def invalidate(cls, inst: object, name: str) -> None: ...
 
