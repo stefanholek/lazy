d6b159641f56eef1adfa0d983772eb0bfe5a4166
Stefan H. Holek <stefan@epy.co.at>
2022-06-19 10:47:41 +0200

    Overload __get__ type signature to reflect different return type when invoked as class attribute.
    https://docs.python.org/3/howto/descriptor.html#descriptor-protocol
    https://mypy.readthedocs.io/en/stable/more_types.html#function-overloading
    https://github.com/python/typeshed/blob/master/stdlib/functools.pyi#L142

--- lazy/lazy.pyi
+++ lazy/lazy.pyi
@@ -1,4 +1,5 @@
 from typing import TypeVar, Callable, Type, Generic
+from typing import Optional, overload
 
 _R = TypeVar("_R")
 _C = TypeVar("_C")
@@ -8,7 +9,11 @@ class lazy(Generic[_R, _C]):
 
     def __init__(self, func: Callable[[_C], _R]) -> None: ...
 
-    def __get__(self, inst: _C, inst_cls: Type[_C]) -> _R: ...
+    @overload
+    def __get__(self, inst: None, inst_cls: Optional[Type[_C]] = ...) -> lazy[_R, _C]: ...
+
+    @overload
+    def __get__(self, inst: _C, inst_cls: Optional[Type[_C]] = ...) -> _R: ...
 
     @classmethod
     def invalidate(cls, inst: _C, name: str) -> None: ...
