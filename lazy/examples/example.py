6137be0088b255eda657701692f7513ccb35731d
Stefan H. Holek <stefan@epy.co.at>
2022-06-19 11:00:15 +0200

    Add more examples for mypy to check.

--- lazy/examples/example.py
+++ lazy/examples/example.py
@@ -1,3 +1,4 @@
+from datetime import date
 from lazy import lazy
 
 
@@ -15,12 +16,26 @@ class C(object):
     def baz(self) -> int:
         return 42
 
+    @lazy
+    def quux(self) -> date:
+        return date(2010, 10, 10)
+
 
 def f() -> None:
     c = C()
     'hello ' + c.foo
     'hello ' + c.bar
     # 'hello ' + c.baz
+    1 + c.baz
+
+    str(c.baz)
+    c.quux.strftime('%y') == '10'
 
     lazy.invalidate(c, 'foo')
+
+    type(C.foo) == lazy
+    type(C.bar) == lazy
+
+    type(c.foo) == str
+    type(c.baz) == int
 
